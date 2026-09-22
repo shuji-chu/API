69959c960f7cf624548ba9a4e8b1b10cc9c8cb3d
xiaochu <chulingli1988@outlook.com>
2026-09-22 17:30:48 +0800

    删除 data-price.js

--- data-price.js
+++
@@ -1,9 +0,0 @@
- cat in allApis) {
-    allApis[cat].forEach(api => {
-      if (priceMap.hasOwnProperty(api.id)) {
-        api.price = priceMap[api.id];
-      }
-    });
-  }
-}
-document.addEventListener('DOMContentLoaded', applyPrice);
